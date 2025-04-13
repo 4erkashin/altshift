699aa8d585f7ba37cf83a88f0c933cf83f0b111f
Iurii <cherkashin.ju@gmail.com>
2025-04-13 16:22:04 +0200

    refactor: switch previews from stubs to actual ls data

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import { ApplicationPreviewCard } from '@/components/application-preview-card';
 import { CreateApplicationButton } from '@/components/create-application-button';
 import { GoalHighlight } from '@/components/goal-highlight';
 import { PageHeader } from '@/components/page-header';
+import { useApplications } from '@/lib/storage';
 import styles from './page.module.css';
 
 export default function Home() {
+  const { applications } = useApplications();
+
   return (
     <main>
       <PageHeader
@@ -15,9 +18,9 @@ export default function Home() {
       </PageHeader>
 
       <div className={styles.applicationPreviews}>
-        <ApplicationPreviewCard />
-        <ApplicationPreviewCard />
-        <ApplicationPreviewCard />
+        {applications.map((application) => (
+          <ApplicationPreviewCard key={application.id} data={application} />
+        ))}
       </div>
 
       <GoalHighlight />
--- components/application-preview-card/ApplicationPreviewCard.tsx
+++ components/application-preview-card/ApplicationPreviewCard.tsx
@@ -1,35 +1,19 @@
 import { CopyToClipboardButton } from '@/components/copy-to-clipboard-button';
 import { DeleteApplicationButton } from '@/components/delete-application-button';
+import { Application } from '@/lib/storage/useApplications';
 import clsx from 'clsx';
 import { FC, HTMLAttributes } from 'react';
 
 import styles from './ApplicationPreviewCard.module.css';
 
-export const ApplicationPreviewCard: FC<HTMLAttributes<HTMLDivElement>> = ({
-  className,
-  ...restProps
-}) => {
+export const ApplicationPreviewCard: FC<
+  HTMLAttributes<HTMLDivElement> & {
+    data: Application;
+  }
+> = ({ className, data, ...restProps }) => {
   return (
     <div className={clsx(styles.root, className)} {...restProps}>
-      <p className={styles.text}>
-        <span className={styles.firstLine}>Dear Stripe team,</span>I am a highly
-        skilled product designer with a passion for creating intuitive,
-        user-centered designs. I have a strong background in design systems and
-        am excited about the opportunity to join the Stripe product design team
-        and work on building out the design system for the platform. I am
-        particularly drawn to Stripe&apos;s mission of making it easy for
-        businesses to sell online and am confident that my experience in
-        creating user-friendly designs will be an asset to the team. I have
-        experience in conducting user research, creating wireframes, and
-        prototyping interactive designs, as well as working closely with
-        engineers to ensure that my designs are implemented correctly. I am a
-        strong collaborator and have experience working in cross-functional
-        teams to bring new products and features to market. I&apos;m confident
-        that I can help improve Stripe&apos;s user experience and make it even
-        more accessible to businesses. I would love the opportunity to speak
-        with you further about my qualifications and how I can contribute to the
-        Stripe team. Thank you for considering my application.
-      </p>
+      <p className={styles.text}>{data.result}</p>
 
       <div className={styles.buttons}>
         <DeleteApplicationButton />
